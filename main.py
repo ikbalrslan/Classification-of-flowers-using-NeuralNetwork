some bugs are fixed about softmax and cross entropy.

main.py
@@ -6,52 +6,42 @@ import numpy as np
 
 
 def programWorkStation():
+
+
     image_values = read_mat("train.mat")[0]
     normalized_images = normalize(image_values)
     expected_classes = read_mat("train.mat")[1]
     expected_outputs = expectedOutputs(expected_classes)
-    #print(expected_outputs)
 
-    #single_layer_network(image_values)
-
-    ############################## TRY ####################################
-
-
-    # X = (hours sleeping, hours studying), y = score on test
     X = normalized_images
-    #y = np.array(([92], [86], [89]), dtype=float)
-    
-    y = expected_outputs / 5  # max test score is 100
 
     size_of_one_image = len(normalized_images[0])
     size_of_input = size_of_one_image
-    hidden_node_number = 3
-    hidden_layer_number = 1
+    hidden_node_number = 200
+    hidden_layer_number = 0
     size_of_output = 5
+
+    Beauty_Neural_Network = Neural_Network(size_of_input, hidden_node_number, size_of_output, hidden_layer_number)
+    Beauty_Neural_Network.trainModel(X, expected_outputs)
+
+
 
     """
     # X = (hours sleeping, hours studying), y = score on test
-    X = np.array(([2, 9], [5,8]), dtype=float)
-    y = np.array(([92], [89]), dtype=float)
+    X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
+    y = np.array(([92], [86], [89]), dtype=float)
 
     # scale units
-    X = X / np.amax(X, axis=0)  # maximum of X array
+    X = X / 10  # maximum of X array
     y = y / 100  # max test score is 100
-    """
+
+    size_of_input = 2
+    hidden_node_number = 3
+    hidden_layer_number = 1
+    size_of_output = 1
     Beauty_Neural_Network = Neural_Network(size_of_input, hidden_node_number, size_of_output, hidden_layer_number)
-
-    print("X: ", X)
-
-    # defining our output
-    #my_outputs = Beauty_Neural_Network.forwardPropagation(X)
-
-    #print("\nPredicted Outputs: \n" + str(my_outputs))
-
-    #soumOfResults = np.apply_along_axis(sum, 1, my_outputs)
-    #print("\nsum of Outputs: \n" + str(soumOfResults))
-    print("\nActual Output: \n" + str(expected_outputs))
-
-    print("\nError Array: ", Beauty_Neural_Network.trainModel(X, expected_outputs))
+    Beauty_Neural_Network.trainModel(X, y)
+    """
 
 programWorkStation()
 

neural_network.py
@@ -11,85 +11,129 @@ class Neural_Network(object):
         self.numberOfHidden = numberOfHidden
         self.weight_matrix_dict = {}
         self.weight_name_list = []
+        self.bias_name_list = []
+        self.bias_dict = {}
+        self.step_outputs = []
+
         np.random.seed(1) # take the same random variables for each initialized matrix
 
         for i in range(self.numberOfHidden + 1):
             weight_matrix_name = "W" + str(i + 1)
+            bias_name = "b" + str(i + 1)
             self.weight_name_list.append(weight_matrix_name)
+            self.bias_name_list.append(bias_name)
 
         # define weights random initially
         for i in range(len(self.weight_name_list)):
             weight_matrix_name = self.weight_name_list[i]
-            if i == 0:
-                self.weight_matrix_dict[weight_matrix_name] = np.random.randn(self.inputSize, self.hiddenSize)
-                #self.W1 = self.weight_matrix_dict[weight_matrix_name]
-            elif i != (len(self.weight_name_list) - 1):
-                self.weight_matrix_dict[weight_matrix_name] = np.random.randn(self.hiddenSize, self.hiddenSize)
-            else:
-                self.weight_matrix_dict[weight_matrix_name] = np.random.randn(self.hiddenSize, self.outputSize)
-                #self.W2 = self.weight_matrix_dict[weight_matrix_name]
+            bias_name = self.bias_name_list[i]
+            if self.numberOfHidden != 0:
+                if i == 0:
+                    self.weight_matrix_dict[weight_matrix_name] = np.random.randn(self.inputSize, self.hiddenSize)
+                    #self.W1 = self.weight_matrix_dict[weight_matrix_name]
+                    self.bias_dict[bias_name] = np.zeros((1, self.hiddenSize))
+                    #print("biasss:  ",self.bias_dict[bias_name])
+                elif i != (len(self.weight_name_list) - 1):
+                    self.weight_matrix_dict[weight_matrix_name] = np.random.randn(self.hiddenSize, self.hiddenSize)
+                    self.bias_dict[bias_name] = np.zeros((1, self.hiddenSize))
+                    #print("biasss:  ",self.bias_dict[bias_name])
+                else:
+                    self.weight_matrix_dict[weight_matrix_name] = np.random.randn(self.hiddenSize, self.outputSize)
+                    #self.W2 = self.weight_matrix_dict[weight_matrix_name]
+                    self.bias_dict[bias_name] = np.zeros((1, self.outputSize))
+                    #print("biasss:  ",self.bias_dict[bias_name])
+            else: # if there is no hidden layer. This means that this is a single layer
+                self.weight_matrix_dict[weight_matrix_name] = np.random.randn(self.inputSize, self.outputSize)
+                # self.W1 = self.weight_matrix_dict[weight_matrix_name]
+                self.bias_dict[bias_name] = np.zeros((1, self.outputSize))
+                # print("biasss:  ",self.bias_dict[bias_name])
 
         for weight_matrix_name in self.weight_name_list:
-            print(weight_matrix_name, self.weight_matrix_dict[weight_matrix_name])
+            print("\n",weight_matrix_name, self.weight_matrix_dict[weight_matrix_name])
+        for bias_name in self.bias_name_list:
+            print("\n",bias_name, self.bias_dict[bias_name])
 
 
+    def forwardPropagation(self, X, expectedOutputs):
+        print("\n\ninput: ", X)
 
-    def forwardPropagation(self, X):
         self.z_result = 0
-        self.sigmoided_z_result = 0
+        self.a_result = 0
         # for each matrix in matrix dictionary, iterate forward propagation
-        for weight_matrix in self.weight_name_list:
-            if weight_matrix == "W1":
-                self.z_result = np.dot(X, self.weight_matrix_dict[weight_matrix]) # forward propagation
-                #print("\nz", self.z_result)
-                self.sigmoided_z_result = self.sigmoid(self.z_result)  # activation function
-                #print("\nsigmoided_z", self.sigmoided_z_result)
-            else:
-                self.z_result = np.dot(self.sigmoided_z_result, self.weight_matrix_dict[weight_matrix])  # forward propagation
-                #print("\nz", self.z_result)
-                self.sigmoided_z_result = self.sigmoid(self.z_result)  # activation function
-                #print("\nsigmoided_z", self.sigmoided_z_result)
-        #return self.sigmoided_z_result
+        weight_list_size = len(self.weight_name_list)
+        for i in range(weight_list_size):
+            wName = self.weight_name_list[i]
+            bName = self.bias_name_list[i]
 
-        """
-        # forward propagation through our network
-        self.z_1 = np.dot(X, self.W1)  # dot product of X (input) and first set of 3x2 weights
-        self.sigmoided_z_1 = self.sigmoid(self.z_1)  # activation function
-        self.z_2 = np.dot(self.sigmoided_z_1, self.W2)  # dot product of hidden layer (z2) and second set of 3x1 weights
-        self.sigmoided_z_2 = self.sigmoid(self.z_2)  # final activation function
-        return self.sigmoided_z_2
-        """
-        #print("z result suan: ", self.sigmoided_z_result)
+            #single layer neural network
+            if weight_list_size == 1: # use softmax as activation function for output
+                self.z_result = np.dot(X, self.weight_matrix_dict[wName]) + self.bias_dict[bName]  # forward propagation
+                self.step_outputs.append(self.z_result)
+            #multilayer neural network
+            else:
+                if wName == "W1":
+                    self.z_result = np.dot(X, self.weight_matrix_dict[wName]) + self.bias_dict[bName] # forward propagation
+                    self.step_outputs.append(self.z_result)
+                    self.a_result = self.sigmoid(self.z_result)  # activation function
+                    self.step_outputs.append(self.a_result)
+
+                elif i == (weight_list_size - 1): # use softmax as activation function for output
+                    self.z_result = np.dot(self.a_result, self.weight_matrix_dict[wName]) + self.bias_dict[bName]  # forward propagation
+                    self.step_outputs.append(self.z_result)
+
+                else:
+                    self.z_result = np.dot(self.a_result, self.weight_matrix_dict[wName]) + self.bias_dict[bName] # forward propagation
+                    self.step_outputs.append(self.z_result)
+                    self.a_result = self.sigmoid(self.z_result)  # activation function
+                    self.step_outputs.append(self.a_result)
 
         # call softmax function for each row of output 2-d numpy array(for each image actually)
-        softmax_results = np.apply_along_axis(self.softmax, 1, self.sigmoided_z_result)
-        print("\n\nsoftmax result: ", softmax_results)
+        softmax_results = self.softmax(self.z_result)
+        self.step_outputs.append(softmax_results)
+
+        #print("toplamına bak: ", sum(softmax_results[16]))
+
+        for i in range(len(self.step_outputs)):
+            print("\nstep output =>",i, self.step_outputs[i])
+
+        self.total_loss(softmax_results, expectedOutputs)
         return softmax_results
 
-    def backwardPropagation(self,softmax_results,expected_output):
-        #self.output_error_results = self.cross_entropy(softmax_results,expected_output)
-        print("\npred Cross: ", softmax_results)
-        print("\nexpect Cross: ", expected_output)
-        self.cross_entropy_results = np.apply_along_axis(self.cross_entropy, 1, softmax_results, expected_output)
-
-        sument = 0
-        print("\ncross_entropy_results: \n")
-        for i in self.cross_entropy_results:
-            sument += i
-            print(i)
-
-        print("sument: ", sument / len(self.cross_entropy_results))
-
-        print("\ncross_entropy_results: \n",self.cross_entropy_results)
-
-        #self.cross_entropy(softmax_results, expected_output)
-        #print(self.output_error_results)
-
+    # takes batch size times image and for each image; calculates probabilities of image outputs
+    def softmax(self, all_image_outputs):
+        softmax_list = []
+        for i in all_image_outputs:
+            exps = []
+            for j in i:
+                exps.append(np.exp(j))
+            exps = np.asarray(exps)
+            sumOfExps = sum(exps)  # take some of exponentials
+            softmax = []
+            for j in exps:
+                softmax.append(j/sumOfExps)
+            softmax = np.asarray(softmax)
+            softmax_list.append(softmax)
+        softmax_list = np.asarray(softmax_list)
+        return softmax_list
 
     def trainModel(self, inputs, expectedOutputs):
-        predicted_outputs = self.forwardPropagation(inputs)
-        self.backwardPropagation(predicted_outputs, expectedOutputs)
+        predicted_outputs = self.forwardPropagation(inputs,expectedOutputs)
+        self.backwardPropagation(inputs, predicted_outputs, expectedOutputs)
 
+    def backwardPropagation(self, X, y, o):
+        print("none!!!!")
+
+        """
+        # backward propagate through the network
+        self.o_error = y - o  # error in output
+        self.o_delta = self.o_error * self.derivate_the_sigmoid(o)  # applying derivative of sigmoid to error
+
+        self.z2_error = self.o_delta.dot(self.W2.T)  # z2 error: how much our hidden layer weights contributed to output error
+        self.z2_delta = self.z2_error * self.derivate_the_sigmoid(self.z2)  # applying derivative of sigmoid to z2 error
+
+        self.W1 += X.T.dot(self.z2_delta)  # adjusting first set (input --> hidden) weights
+        self.W2 += self.z2.T.dot(self.o_delta)  # adjusting second set (hidden --> output) weights
+        """
 
     # activation sigmoid function
     def sigmoid(self, z):
@@ -101,13 +145,44 @@ class Neural_Network(object):
         result = z * (1 - z)
         return result
 
-    def softmax(self, outputs):
-        exps = [np.exp(i) for i in outputs] # take exp value for each element of outputs
-        exps = np.asarray(exps)
-        sumOfExps = sum(exps) # take some of exponentials
-        softmax = [j/sumOfExps for j in exps] # for each element of exps, divide element to sum of exps
-        softmax = np.asarray(softmax)
-        return softmax
+    def total_loss(self, softmax_results, expected_output):
+        print("\nsoftmax_results of total: ", softmax_results)
+        print("\nexpect of total: ", expected_output)
+
+        self.cross_entropy_results = self.cross_entropy(softmax_results,expected_output)
+
+        loss_sum = 0
+        #print("\ncross_entropy_results: \n")
+        for i in self.cross_entropy_results:
+            loss_sum += i
+            #print(i)
+
+        total_loss = loss_sum / len(self.cross_entropy_results)
+        print("\ncross_entropy_results and size: \n", self.cross_entropy_results, len(self.cross_entropy_results))
+        print("\nLoss: \n", total_loss)
+
+        # self.cross_entropy(softmax_results, expected_output)  # print(self.output_error_results)
+
+    # E = – ∑ ci . log(pi) + (1 – ci ). log(1 – pi)
+    # this function returns error list. one index for each image(index is mean of output errors for each image)
+    def cross_entropy(self, softmax_results, expected_output):
+        output_node_number = len(expected_output[0])
+        error_list = []
+        for i in range(len(softmax_results)):
+            errorSum = 0
+            for j in range(output_node_number):
+                pre_ = softmax_results[i][j]
+                exp_ = expected_output[i][j]
+                #print("\npre: ",pre_,"exp: ",exp_)
+                errorSum += (exp_ * log10(pre_) + (1 - exp_) * log10(1 - pre_))
+            result = - errorSum
+            mean_of_errors = result / output_node_number
+            error_list.append(mean_of_errors)
+        error_list = np.asarray(error_list)
+        return error_list
+
+    def derivate_the_cross_entropy(self, predict, actual):
+        return - actual / predict
 
     #soft_max = softmax(x)
     def derivate_the_softmax(self,soft_max):
@@ -115,43 +190,6 @@ class Neural_Network(object):
         s = soft_max.reshape(-1, 1)
         return np.diagflat(s) - np.dot(s, s.T)
 
-    def cross_entropy(self, predictOutputs, expectedOutput):
-        errorSum = 0
-        for i in range(len(predictOutputs)):
-            errorSum += (expectedOutput[i] * log10(predictOutputs[i]) + (1 - expectedOutput[i]) * log10(1 - predictOutputs[i]))
-
-        result = - errorSum
-        sum_of_vector = sum(result)
-        output_node_number = len(expectedOutput[0])
-        mean_of_errors = sum_of_vector / output_node_number
-        return mean_of_errors
-
-    def derivate_the_cross_entropy(self, predict, actual):
-        return - actual / predict
 
 
 
-    """
-    def cross_entropy(self, predicted, expected):
-        print("\n\npredicted: ", predicted)
-        print("\n\nexpected: ", expected)
-        print("\n\nDifference: ", predicted - expected)
-
-
-        X is the output from fully connected layer (num_examples x num_classes)
-        y is labels (num_examples x 1)
-        	Note that y is not one-hot encoded vector.
-        	It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
-
-
-        m = expected.shape[0]
-        print("M: ", m)
-
-        # We use multidimensional array indexing to extract
-        # softmax probability of the correct label for each sample.
-        log_likelihood = -np.log(predicted[range(m), expected])
-        print(log_likelihood)
-
-        loss = np.sum(log_likelihood) / m
-        return loss
-    """